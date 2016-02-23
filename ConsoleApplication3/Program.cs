8a89b5dfe684444e74def6e14a2a06d073abe86b
user <user@mail.com>
2016-02-23 06:20:21 -0500

    Completed Parabolas01 method

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -14,58 +14,68 @@ namespace ConsoleApplication3
   {
     static void Main(string[] args)
     {
-      DrawParabola01(2900);
+      for (int i = 0; i < 1; i++)
+        DrawParabolas01(1000, String.Format("parabola{0}", i));
     }
 
-    private static void DrawParabola01(int a)
+    private static void DrawParabolas01(int a, string fileName)
     {
       Random rnd = new Random();
       Bitmap bitmap = new Bitmap(a, a);
       Graphics g = Graphics.FromImage(bitmap);
 
-      // create new random pen
       Color color = new Color();
       Pen pen = new Pen(color);
-      g.FillRectangle(Brushes.Black, 0, 0, a, a); // fill background
-      for (int i = 0; i < a / 10; i++)
-      {
-        color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256), rnd.Next(256));
-        pen.Color = color;
-        g.DrawLine(pen, 0, i * 10, 10 + ((i + 1) * 10), a); // bottom-left
-        g.DrawLine(pen, i * 10, 0, a, 10 + ((i + 1) * 10)); // top-right
-      }
+
+      // fill background
       color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256), rnd.Next(256));
-      pen.Color = color; 
-      
-      for (int i = 0; i < a / 2; i += 5)
-      {
-        g.DrawLine(pen, i, a / 2, a / 2, a / 2 - i); //top-left
-      }
+      SolidBrush brush = new SolidBrush(color);
+      g.FillRectangle(brush, 0, 0, a / 2, a / 2);
+
+      color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256), rnd.Next(256));
+      brush.Color = color;
+      g.FillRectangle(brush, a / 2, 0, a / 2, a / 2);
+
+      color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256), rnd.Next(256));
+      brush.Color = color;
+      g.FillRectangle(brush, 0, a / 2, a / 2, a / 2);
+
+      color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256), rnd.Next(256));
+      brush.Color = color;
+      g.FillRectangle(brush, a / 2, a / 2, a / 2, a / 2);
+
+
+        for (int i = 0; i < a; i += 10)
+        {
+          color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256), rnd.Next(256));
+          pen.Color = color;
+          g.DrawLine(pen, 0, i, i, a); // bottom-left
+          g.DrawLine(pen, i, 0, a, i); // top-right
+          g.DrawLine(pen, i, a, a, a - i); //bottom-right
+          g.DrawLine(pen, 0, a - i, i, 0); //top-left
+        }
+
       color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256), rnd.Next(256));
       pen.Color = color;
       for (int i = 0; i < a / 2; i += 5)
-      {
+        g.DrawLine(pen, i, a / 2, a / 2, a / 2 - i); //top-left
+
+      color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256), rnd.Next(256));
+      pen.Color = color;
+      for (int i = 0; i < a / 2; i += 5)
         g.DrawLine(pen, a / 2, a - i, a / 2 + i, a / 2); //bottom-right
-      }
-      color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256), rnd.Next(256));
-      pen.Color = color; 
-      for (int i = 0; i < a / 2; i += 5)
-      {
 
+      color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256), rnd.Next(256));
+      pen.Color = color;
+      for (int i = 0; i < a / 2; i += 5)
         g.DrawLine(pen, a / 2, i, a / 2 + i, a / 2); //top-right
-      }
+
       color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256), rnd.Next(256));
-      pen.Color = color; 
+      pen.Color = color;
       for (int i = 0; i < a / 2; i += 5)
-      {
-
         g.DrawLine(pen, i, a / 2, a / 2, a / 2 + i); //bottom-left
-      }
 
-
-
-
-      bitmap.Save("parabola01.png", System.Drawing.Imaging.ImageFormat.Png);
+      bitmap.Save(String.Format("{0}.png", fileName), System.Drawing.Imaging.ImageFormat.Png);
     }
   }
 }
